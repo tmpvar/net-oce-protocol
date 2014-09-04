2b1a7504d0f606961b7ae581d8c420f5b7f97c98
Elijah Insua <tmpvar@gmail.com>
2014-09-04 13:54:04 -0700

    unpack objects

--- response.js
+++ response.js
@@ -25,38 +25,61 @@ function copy(src)  {
   return dst;
 }
 
+function mapValues(value, ret, fn) {
+  ret = ret || [];
+
+  if (value.type < 16) {
+      ret.push(value[type_mapping[value.type]]);
+  } else {
+    switch (value.type) {
+      case 16: // OPERATION
+      break;
+
+      case 17: // ERROR
+        fn(new Error(value.string_value));
+        return null;
+      break;
+
+      case 18: // SHAPE_HANDLE
+        ret.push({ id: value.uint32_value });
+      break;
+
+      case 19:
+        ret.push(new Float32Array(copy(value.bytes_value)));
+      break;
+
+      case 20:
+        ret.push(new Float64Array(copy(value.bytes_value)));
+      break;
+
+      case 21:
+        var l = value.item.length;
+        var pack = {};
+
+        for (var i=0; i<l; i++) {
+          var subvalue = value.item[i];
+          var collected = mapValues(subvalue, [], fn);
+          if (collected) {
+            pack[subvalue.string_value] = collected[0];
+          }
+        }
+        ret.push(pack);
+
+      break;
+    }
+  }
+  return ret;
+}
+
 function getResponseArray(obj, fn) {
   var ret = [];
 
   var l = obj.value.length;
   for (var i=0; i<l; i++) {
-    var value = obj.value[i];
-
-    if (value.type < 16) {
-      ret.push(value[type_mapping[value.type]]);
-    } else {
-      switch (value.type) {
-        case 16: // OPERATION
-        break;
-
-        case 17: // ERROR
-          return fn(new Error(value.string_value));
-        break;
-
-        case 18: // SHAPE_HANDLE
-          ret.push({ id: value.uint32_value });
-        break;
-
-        case 19:
-          ret.push(new Float32Array(copy(value.bytes_value)));
-        break;
-
-        case 20:
-          ret.push(new Float64Array(copy(value.bytes_value)));
-        break;
-      }
+    if (mapValues(obj.value[i], ret, fn) === null) {
+      return;
     }
   }
 
-  fn(null, (ret.length > 1) ? ret : ret[0]);
+  fn && fn(null, (ret.length > 1) ? ret : ret[0]);
 }
